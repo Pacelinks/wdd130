df019b2c998a7ae8162e4dd1ce29c14d6259e958
pacelinks <ojo23002@byui.edu>
2023-09-22 11:28:06 +0100

    first commit

---
+++ Untitled-1.py
@@ -0,0 +1,47 @@
+def display_numbers(x, y):
+    print(x)
+
+x = 3
+y = 4
+
+display_numbers(x, y)
+
+def display_numbers(x, y):
+
+    print(x)
+
+x = 3
+
+y = 4
+
+display_numbers(y, x)
+
+def display_numbers(x, y):
+
+    x = 10
+
+x = 3
+
+y = 4
+
+display_numbers(x, y)
+
+print(x)
+
+
+
+def display_numbers(x, y):
+    return 10
+
+x = 3
+y = 4
+x = display_numbers(x, y)
+
+print(x)
+
+    #print(x)
+def display_numbers(x, y):
+    display_numbers(3, 4)
+    print(x)
+
+    
